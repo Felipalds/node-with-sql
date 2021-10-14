095cc9d54412c5b0653e249a13a537ad65fc5368
Felipalds <luizfelipefonsecarosa@gmail.com>
2021-10-14 19:14:19 -0300

    Adding Express

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql')
+const express = require('express')
+const app = express()
 
 const conn = mysql.createConnection({
     host: "localhost",
@@ -6,12 +8,34 @@ const conn = mysql.createConnection({
     password: "root",
     database: "mydb"
 })
+app.use(
+    express.urlencoded({
+    extended: true
+  })
+)
 
-conn.connect((err) => {
-    if(err) throw err
-    console.log("Database connected!")
-    conn.query("CREATE TABLE people (name VARCHAR (255), age TINYINT(3))", (err, result)=>{
-        if(err) throw err
-        console.log("Table created")
-    })
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    if(req.body && conn){
+        if(req.body.name && req.body.age){
+            const query = `INSERT INTO people (name, age) VALUES ('${req.body.name}', ${req.body.age})`
+            conn.query(query, (err, result) => {
+                if(err) throw err
+                console.log("Success")
+            })
+            res.status(200).json({
+                status: "OK"
+            })
+        } else {
+            res.status(400).json({
+                error: "Something missing"
+            })
+        }
+        
+    }
+})
+
+app.listen(8000, (req, res) => {
+    console.log('Olá')
 })
